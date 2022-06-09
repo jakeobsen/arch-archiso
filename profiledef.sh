d11b873b33bf35d562b1589abf50fa744fae5294
Morten Jakobsen <morten@jakeobsen.com>
2022-06-09 15:33:39 +0100

    Change iso label

--- profiledef.sh
+++ profiledef.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2034
 
 iso_name="unlab_archlinux"
-iso_label="ARCH_$(date +%Y%m)"
+iso_label="ARCHISO"
 iso_publisher="Arch Linux <https://archlinux.org>"
 iso_application="Arch Linux Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
