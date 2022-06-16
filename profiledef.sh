5e7cea1f0210f82cc37eadc6feecc130ce8eb95c
Morten Jakobsen <morten@jakeobsen.com>
2022-06-17 00:02:17 +0100

    Update profile

--- profiledef.sh
+++ profiledef.sh
@@ -21,4 +21,7 @@ file_permissions=(
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
   ["/usr/local/sbin/bootstrap"]="0:0:755"
+  ["/usr/local/share/unlab/"]="0:0:755"
+  ["/usr/local/share/unlab/ssh/"]="0:0:755"
+  ["/usr/local/share/unlab/ssh/*"]="0:0:644"
 )
